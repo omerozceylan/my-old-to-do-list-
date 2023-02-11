5a0a4c3f8157661ea3fd955ac56ff006fdb86f92
Omer Ozceylan <omerozceylan40@gmail.com>
2023-02-11 10:11:07 +0300

    Rewrite: Javascript

--- app.js
+++ app.js
@@ -1,81 +1,23 @@
-let form = document.querySelector('#form')
-let arrayToDo = JSON.parse(localStorage.getItem('toDoArray')) 
-let isPageRefresh = false
-function atStart(){
-    if(isPageRefresh==false){
-        if(!arrayToDo){
-            localStorage.setItem('toDoArray', JSON.stringify([]))
-            isPageRefresh=true
-        }else{
-            arrayToDo.forEach(element => {
-            createItem(element)
-            })  
-        }
-    }
+let formNode = document.querySelector('#form')
+let inputNode = document.querySelector('#input')
+let todoList = document.querySelector('#theToDoList')
 
-    console.log('array : '+ arrayToDo);
+formNode.addEventListener("submit", getInput)
+
+function getInput(event){
+    event.preventDefault()
+    const listCount = todoList.getElementsByTagName("li").length+1
+    if(inputNode.value==="" || listCount==7) return 
+    createElement(inputNode.value)
 }
-atStart()
 
-form.addEventListener('submit', function(event){
-        event.preventDefault()
-        let input = document.querySelector('#input')
-        if(!input.value == ""){
-            let text = input.value
-            console.log(text)
-            arrayToDo.push(text)
-            localStorage.setItem('toDoArray',JSON.stringify(arrayToDo))
-            createItem(text)
-        }
-        input.value = ""
-})
+function createElement(text){
+    inputNode.value = ""
+    let html = `<li class="toDoElement">${text}<button class="clearButton" onclick="deleteItem(this)">x</button></li>`
+    todoList.insertAdjacentHTML("beforeend", html)
+}
 
-function createItem(toDoElement){
-        console.log('hey');
-        let theToDoList = document.querySelector('#theToDoList')
-        let listElement = document.createElement('li')
-        listElement.classList.add('element')
-        listElement.innerHTML = `<li class="toDoElement"><div class="innerText">${toDoElement}</div><button class="clearButton">X</button></li>`
-        let everythingInList = document.querySelectorAll('.element')
-        
-        
-        theToDoList.appendChild(listElement)
-        
-        let clearButton = document.querySelectorAll('.clearButton')
-        const currentArray = JSON.parse(localStorage.getItem('toDoArray'))
-        clearButton.forEach((element, num)=> {
-            element.addEventListener('click', function(){
-            const index = Array.prototype.indexOf.call(everythingInList, this.parentNode);
-            currentArray.splice(index,1)
-            element.parentNode.remove()
-            localStorage.setItem('toDoArray', JSON.stringify(currentArray))
-            console.log(currentArray);
-            
-            })
-        })
-        
-        let textClick = document.querySelectorAll('.innerText')
-        let isClicked = false
-        textClick.forEach((element)=>{
-            element.addEventListener('click', function(){
-                if(isClicked==false){
-                element.style.textDecoration='line-through'
-                isClicked=true
-                }else{
-                element.style.textDecoration='none'
-                isClicked=false
-                }
-            })
-        })
-        
-        let deleteBox = document.querySelector('#deleteBox')
-        let deleteButton = document.createElement('button')
-        deleteButton.classList.add('deleteButton')
-        deleteButton.innerHTML='delete'
-       
-     
-
-    }
-    
-    
+function deleteItem(item){
+    item.parentElement.remove()
+}
 
